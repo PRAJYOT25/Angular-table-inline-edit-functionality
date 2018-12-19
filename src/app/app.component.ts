720b0e543e1b8f1dfbb01cfb8fad72c90a9b5224
PRAJYOT-PC\PRAJYOT <prajyot@bittint.com>
2018-12-19 13:19:53 +0530

    table inline edit functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component,OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  isHidden:Boolean;
+  i = 1;
+  editCache = {};
+  dataSet = [];
   title = 'angular-proj';
+  startEdit(): void {
+    this.isHidden=true;
+    let key;
+    for (let i = 0; i < 5; i++) {
+      key=i.toString(),
+    this.editCache[ key ].edit = true;
+    }
+  }
+
+  cancelEdit(): void {
+    this.isHidden=false;
+    let key;
+    for (let i = 0; i < 5; i++) {
+      key=i.toString(),
+    this.editCache[ key ].edit = false;}
+  }
+
+  saveEdit(): void {
+    let key;
+    this.isHidden=false
+    for (let i = 0; i < 5; i++) {
+      key=i.toString();
+    const index = this.dataSet.findIndex(item => item.key === key);
+    Object.assign(this.dataSet[ index ], this.editCache[ key ].data);
+    // this.dataSet[ index ] = this.editCache[ key ].data;
+    this.editCache[ key ].edit = false;
+    }
+  }
+
+  updateEditCache(): void {
+    this.dataSet.forEach(item => {
+      if (!this.editCache[ item.key ]) {
+        this.editCache[ item.key ] = {
+          edit: false,
+          data: { ...item }
+        };
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    for (let i = 0; i < 5; i++) {
+      this.dataSet.push({
+        key    : i.toString(),
+        name   : `Edrward ${i}`,
+        age    : 32,
+        address: `London Park no. ${i}`
+      });
+    }
+    this.updateEditCache();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
